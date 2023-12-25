5564952cd1c6d3841972a4641e34040f2cba562a
McSmart <ofoma_chijioke@yahoo.com>
2023-12-25 11:19:09 +0100

    added a parameter

--- TestAPIOne/TestAPIOne/Controllers/WeatherForecastController.cs
+++ TestAPIOne/TestAPIOne/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace TestAPIOne.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get(string weather)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
